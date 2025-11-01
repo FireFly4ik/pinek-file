e11241b3bf3d2b6e8fad46e5a265005a4f34e05c
FireFly4ik <andreybatman06@gmail.com>
2025-11-01 17:20:13 +0300

    fix: checkIds and ids of service with multi-instances (added address to id)

--- internal/consul/consul.go
+++ internal/consul/consul.go
@@ -27,8 +27,8 @@ func NewProvider(envConf *config.Config) *ConsulProvider {
 		address: envConf.Consul.Address,
 		client:  client,
 		name:    envConf.Consul.Name,
-		checkId: envConf.Consul.CheckId + "(" + envConf.Port + ")",
-		id:      envConf.Consul.Name + "(" + envConf.Port + ")",
+		checkId: envConf.Consul.CheckId + "(" + envConf.Address + ":" + envConf.Port + ")",
+		id:      envConf.Consul.Name + "(" + envConf.Address + ":" + envConf.Port + ")",
 	}
 
 	err = cp.registerService(envConf)
@@ -47,7 +47,7 @@ func (p *ConsulProvider) registerService(envConf *config.Config) error {
 		DeregisterCriticalServiceAfter: envConf.Consul.DeregisterTTL,
 		TTL:                            envConf.Consul.RegisterTTL,
 		TLSSkipVerify:                  true,
-		CheckID:                        envConf.Consul.CheckId + "(" + envConf.Port + ")",
+		CheckID:                        envConf.Consul.CheckId + "(" + envConf.Address + ":" + envConf.Port + ")",
 	}
 
 	port, _ := strconv.Atoi(envConf.Port)
@@ -55,7 +55,7 @@ func (p *ConsulProvider) registerService(envConf *config.Config) error {
 	register := &api.AgentServiceRegistration{
 		Address: envConf.Address,
 		Port:    port,
-		ID:      envConf.Consul.Name + "(" + envConf.Port + ")",
+		ID:      envConf.Consul.Name + "(" + envConf.Address + ":" + envConf.Port + ")",
 		Name:    envConf.Consul.Name,
 		Tags:    []string{"file"},
 		Check:   check,
